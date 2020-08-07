db3e34fca042c071f52793eaeb1bc83e36d32f64
arun <arun@example.com>
2020-08-07 20:51:35 +0530

    initial commit

---
+++ phrasecheck.ps1
@@ -0,0 +1,22 @@
+﻿Param
+(
+    [string] $fileName,
+    [string] $phrase,
+    [int] $threshold=1
+)
+
+$allText = Get-Content -Path $fileName
+$matches=  Select-String -InputObject $allText -Pattern $phrase -AllMatches
+$matchCount = $matches.Matches.Count
+
+Write-Output $matches.Matches.Count
+
+
+if ($matchCount -gt $threshold)
+{
+    throw "$filename includes $matchCount occurances of $phrase. Threshold i s $threshold. "
+}
+else
+{
+    Write-Output "$filename includes no more than $threshold occurances of $phrase. OK!"
+}
